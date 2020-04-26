deb0adf767cb1b138d8a7d5777720cf289816365
Dmitry Sazonov <seoweb@inbox.ru>
2020-04-26 17:01:25 +0300

    my-tree с потомками

--- webcomponents/src/components/my-leaf/my-leaf.js
+++ webcomponents/src/components/my-leaf/my-leaf.js
@@ -1,11 +1,24 @@
-import { LitElement, html } from 'lit-element';
+import { LitElement, html, css } from 'lit-element';
 
 class MyLeaf extends LitElement {
+  static get styles() {
+    return css`
+      span {
+        display: inline-block;
+        padding: 2px 5px;
+        color: #161;
+        background: #c2f5c2;
+        border: #161 1px solid;
+        border-radius: 4px;
+      }
+    `;
+  }
+
   render() {
     return html` 
-      <div>
-        leaf
-      </div>
+      <span>
+        <slot></slot>
+      </span>
     `;
   }
 }
--- webcomponents/src/components/my-tree/my-tree.js
+++ webcomponents/src/components/my-tree/my-tree.js
@@ -1,4 +1,4 @@
-import { LitElement, html } from 'lit-element';
+import { LitElement, html, css } from 'lit-element';
 import '../my-leaf/my-leaf';
 
 class MyTree extends LitElement {
@@ -10,16 +10,49 @@ class MyTree extends LitElement {
     };
   }
 
+  static get styles() {
+    return css`
+      div {
+        margin: 2px 0;
+        padding: 2px 5px;
+        color: #5d2e08;
+        background: #ecdbcd;
+        border: #5d2e08 1px solid;
+        border-radius: 4px;
+      }
+    `;
+  }
+
   constructor() {
     super();
     this.treeData = {};
+    this.tree = '';
+
+    this.makeTree();
+  }
+
+  updated() {
+    this.makeTree();
+  }
+
+  makeTree() {
+    this.tree = '';
+
+    for (let [key, value] of Object.entries(this.treeData)) {
+      if (Array.isArray(value)) {
+        value.map((item) => {
+          this.tree = html`${this.tree} <my-tree treedata="${JSON.stringify(item)}"></my-tree>`;
+        });
+      } else {
+        this.tree = html`${this.tree} <my-leaf>${key}: ${value}</my-leaf>`;
+      }
+    }
   }
 
   render() {
     return html` 
       <div>
-        ${this.treeData.id}
-        <my-leaf></my-leaf>
+        ${this.tree}
       </div>
     `;
   }
