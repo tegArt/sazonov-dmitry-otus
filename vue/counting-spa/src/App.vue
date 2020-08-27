18ac281a77fb6040eadf539f26e70049c8a0f54e
Dmitry Sazonov <seoweb@inbox.ru>
2020-08-27 23:47:10 +0300

    Подготовить общую структуру приложения - компоненты контейнеры для страниц приложения. Сделать первую страницу приложения - форму настроек.

---
+++ vue/counting-spa/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <PageSettings />
+  </div>
+</template>
+
+<script>
+import PageSettings from './components/PageSettings/PageSettings.vue';
+
+export default {
+  name: 'App',
+  components: {
+    PageSettings,
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  width: 360px;
+  margin: 0 auto;
+  padding: 20px;
+
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
